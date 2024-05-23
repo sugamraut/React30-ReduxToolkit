779fde97b1d8b2f719874e385dea43f178dbd840
Sugam Raut <sugamrautbim@gmail.com>
2024-05-23 21:03:19 +0545

    learn about useSelector hook

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import Form from './components/form/Form'
 import { register } from '../../../store/authSlice'
-import { useDispatch } from 'react-redux'
+import { useDispatch,useSelector } from 'react-redux'
+import STATUSES from '../../globals/status/statuses'
+import { useNavigate } from 'react-router-dom'
 
 const Register = () => {
+  const navigate=useNavigate()
+  const {status}=useSelector((state)=> state.auth)
   const dispatch =useDispatch()
  const handleRegister=(data)=>{
   dispatch(register(data))
+  //check the status value
+  //status -->vlaue success-->navigate the to login page.
+  if(status=== STATUSES.SUCCESS){
+    return navigate('/login')
+  }
+  else{
+    return navigate('/register')
+  }
  }
   return (
     <Form type='Register'onSubmit={handleRegister}/>
--- store/authSlice.js
+++ store/authSlice.js
@@ -1,58 +1,40 @@
-import{createSlice} from '@reduxjs/toolkit'
+import {createSlice} from '@reduxjs/toolkit'
 import axios from 'axios'
 import STATUSES from '../src/globals/status/statuses'
 
-const authSlice=createSlice({
-    name :'auth',
-    initialState :{
-        user: null,
-        token: null,
-        status: null
+const authSlice = createSlice({
+    name : 'auth',
+    initialState : {
+        user : null, 
+        token : null, 
+        status : null
     },
-    reducers :{
-            setStatus(state,action){
-                state.status=action.payload
-            },
-            setUser(state,action){
-                state.status=action.payload
-            },
-            setToken(state,action){
-                state.status=action.payload
-            }
+   reducers : {
+    setStatus(state,action){
+        state.status = action.payload
+    },
+    setUser(state,action){
+        state.user = action.payload
+    },
+    setToken(state,action){
+        state.token = action.payload
     }
+   } 
 })
 
-export const {setStatus,setUser,setToken} = authSlice.actions
+export const {setStatus,setUser,setToken} = authSlice.actions 
 export default authSlice.reducer
 
 export function register(data){
     return async function registerThunk(dispatch){
-     dispatch(setStatus(STATUSES.LOADING))
-        try {
-            const response= await axios.post('https://react30.onrender.com/api/user/register',data)
-            if(response.status===200){
-                dispatch(setUser(data))
-             dispatch(setStatus(STATUSES.SUCCESS))
-            }
-            else{
-              dispatch(setStatus(STATUSES.ERROR))
-            }
-        } catch (error) {
-            dispatch(setStatus(STATUSES.ERROR))
-        }
-    }
-}
-
-export function login(data){
-    return async function loginThunk(dispatch){
         dispatch(setStatus(STATUSES.LOADING))
         try {
-            const response = await axios.post("https://react30.onrender.com/api/user/login",data)
-            if(response.status===200 && response.data.token){
-                dispatch(setStatus(STATUSES.SUCCESS))
-                dispatch(setToken(response.data.token))
-            }
-            else{
+            const response =  await axios.post('https://react30.onrender.com/api/user/register',data)
+            if(response.status === 201){
+             dispatch(setUser(data))
+          
+             dispatch(setStatus(STATUSES.SUCCESS))
+            }else{
                 dispatch(setStatus(STATUSES.ERROR))
             }
         } catch (error) {
@@ -61,3 +43,20 @@ export function login(data){
     }
 }
 
+
+export function login(data){
+    return async function loginThunk(dispatch){
+        dispatch(setStatus(STATUSES.LOADING))
+     try {
+        const response =  await axios.post("https://react30.onrender.com/api/user/login",data)
+        if(response.status === 200 && response.data.token){
+            dispatch(setToken(response.data.token))
+            dispatch(setStatus(STATUSES.SUCCESS))
+        }else{
+            dispatch(setStatus(STATUSES.ERROR))
+        }
+     } catch (error) {
+        dispatch(setStatus(STATUSES.ERROR))
+     }
+    }
+}
